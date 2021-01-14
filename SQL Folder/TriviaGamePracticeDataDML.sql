68710c8bd7af399aca426161361981519e87f8b6
shanat24 <62306912+shanat24@users.noreply.github.com>
2021-01-14 15:35:42 -0500

    newst updates, removal of username table 3:35

--- SQL Folder/TriviaGamePracticeDataDML.sql
+++ SQL Folder/TriviaGamePracticeDataDML.sql
@@ -1,27 +1,27 @@
 Use TriviaGameDDL;
 
-DESC Username;
+-- DESC Username;
 
-INSERT INTO Username (username)
-	VALUES("Sandy1"),
-    ("Claudia1"),
-    ("Tyler1"),
-    ("Daniel1"),
-    ("Shantoria1");
+-- INSERT INTO Username (username)
+-- 	VALUES("Sandy1"),
+--     ("Claudia1"),
+--     ("Tyler1"),
+--     ("Daniel1"),
+--     ("Shantoria1");
     
 DESC Quiz;
 
-INSERT INTO Quiz (QuizId, NumberCorrect, TotalQuestions, Username_username)
-VALUES (1, 5, 10, "Sandy1"),
-(2, 6, 10, "Claudia1"),
-(3, 7, 10, "Tyler1"),
-(4, 8, 10, "Daniel1"),
-(5, 9, 10, "Shantoria1"),
-(6, 10, 10, "Sandy1"),
-(7, 1, 10, "Claudia1"),
-(8, 2, 10, "Tyler1"),
-(9, 3, 10, "Daniel1"),
-(10, 4, 10, "Shantoria1"),
-(11, 5, 10, "Sandy1"); 
+INSERT INTO Quiz (QuizId, NumberCorrect, TotalQuestions)
+VALUES (1, 5, 10),
+(2, 6, 10),
+(3, 7, 10),
+(4, 8, 10),
+(5, 9, 10),
+(6, 10, 10),
+(7, 1, 10),
+(8, 2, 10),
+(9, 3, 10),
+(10, 4, 10),
+(11, 5, 10); 
 
 SELECT * FROM QUIZ
--- SQL Folder/TriviaGameTestsDDL.sql
+++ SQL Folder/TriviaGameTestsDDL.sql
@@ -5,21 +5,21 @@ DROP SCHEMA IF EXISTS TriviaGameTestsDDL;
 CREATE SCHEMA IF NOT EXISTS TriviaGameTestsDDL;
 USE TriviaGameTestsDDL;
 
-CREATE TABLE IF NOT EXISTS Username (
-  username VARCHAR(45),
-  PRIMARY KEY (username),
-  UNIQUE INDEX username_UNIQUE (username ASC) VISIBLE)
-;
+-- CREATE TABLE IF NOT EXISTS Username (
+--   username VARCHAR(45),
+--   PRIMARY KEY (username),
+--   UNIQUE INDEX username_UNIQUE (username ASC) VISIBLE)
+-- ;
 
 
 CREATE TABLE IF NOT EXISTS Quiz (
   QuizId INT NOT NULL,
   NumberCorrect INT NOT NULL,
   TotalQuestions INT NOT NULL,
-  Score DOUBLE AS (NumberCorrect / TotalQuestions * 100),
-  Username_username VARCHAR(45),
-    FOREIGN KEY (Username_username)
-    REFERENCES TriviaGameTestsDDL.Username (username)
-   )
+  Score DOUBLE AS (NumberCorrect / TotalQuestions * 100))
+--   Username_username VARCHAR(45),
+--     FOREIGN KEY (Username_username)
+--     REFERENCES TriviaGameTestsDDL.Username (username)
+--    )
 ;
 
