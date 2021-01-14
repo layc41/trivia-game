3f2bc34561a71894a4c20c74014a0575d422f5af
shanat24 <62306912+shanat24@users.noreply.github.com>
2021-01-14 15:19:48 -0500

    updated test DDL

--- SQL Folder/TriviaGameTestsDDL.sql
+++ SQL Folder/TriviaGameTestsDDL.sql
@@ -6,7 +6,7 @@ CREATE SCHEMA IF NOT EXISTS TriviaGameTestsDDL;
 USE TriviaGameTestsDDL;
 
 CREATE TABLE IF NOT EXISTS Username (
-  username VARCHAR(45) NOT NULL,
+  username VARCHAR(45),
   PRIMARY KEY (username),
   UNIQUE INDEX username_UNIQUE (username ASC) VISIBLE)
 ;
@@ -16,9 +16,8 @@ CREATE TABLE IF NOT EXISTS Quiz (
   QuizId INT NOT NULL,
   NumberCorrect VARCHAR(45) NOT NULL,
   TotalQuestions VARCHAR(45) NOT NULL,
-  Score VARCHAR(45) NOT NULL,
-  Username_username VARCHAR(45) NOT NULL,
-  PRIMARY KEY (QuizId, Username_username),
+  Score DOUBLE AS (NumberCorrect / TotalQuestions * 100),
+  Username_username VARCHAR(45),
     FOREIGN KEY (Username_username)
     REFERENCES TriviaGameTestsDDL.Username (username)
    )
