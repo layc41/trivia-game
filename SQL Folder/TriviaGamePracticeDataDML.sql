f2fbe0561b4283a9801b0ad6a5bd65fa8f1ddc39
shanat24 <62306912+shanat24@users.noreply.github.com>
2021-01-14 15:06:04 -0500

    New DDL and DML with calculated field

--- SQL Folder/TriviaGamePracticeDataDML.sql
+++ SQL Folder/TriviaGamePracticeDataDML.sql
@@ -11,15 +11,17 @@ INSERT INTO Username (username)
     
 DESC Quiz;
 
-INSERT INTO Quiz (QuizId, NumberCorrect, TotalQuestions, Score, Username_username)
-VALUES (1, 5, 10, 50, "Sandy1"),
-(2, 6, 10, 50, "Claudia1"),
-(3, 7, 10, 70, "Tyler1"),
-(4, 8, 10, 80, "Daniel1"),
-(5, 9, 10, 90, "Shantoria1"),
-(6, 10, 10, 100, "Sandy1"),
-(7, 1, 10, 10, "Claudia1"),
-(8, 2, 10, 20, "Tyler1"),
-(9, 3, 10, 30, "Daniel1"),
-(10, 4, 10, 40, "Shantoria1"),
-(11, 5, 10, 50, "Sandy1"); 
+INSERT INTO Quiz (QuizId, NumberCorrect, TotalQuestions, Username_username)
+VALUES (1, 5, 10, "Sandy1"),
+(2, 6, 10, "Claudia1"),
+(3, 7, 10, "Tyler1"),
+(4, 8, 10, "Daniel1"),
+(5, 9, 10, "Shantoria1"),
+(6, 10, 10, "Sandy1"),
+(7, 1, 10, "Claudia1"),
+(8, 2, 10, "Tyler1"),
+(9, 3, 10, "Daniel1"),
+(10, 4, 10, "Shantoria1"),
+(11, 5, 10, "Sandy1"); 
+
+SELECT * FROM QUIZ
